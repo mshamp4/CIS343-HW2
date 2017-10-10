efb06b46c0c81e606cfc9fa7491b215415c6b0ce
Matt <mshamp4@users.noreply.github.com>
2017-10-10 10:59:12 -0400

    Create Concert.h

---
+++ Concert.h
@@ -0,0 +1,27 @@
+#ifndef _H_CONCERT_
+#define _H_CONCERT_
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <ctime>
+
+class Concert {
+
+        public:
+        Concert();
+        Concert(std::string name, std::vector<std::string> pFriends, int pDesire, std::tm* pDate);
+
+        friend std::string friendsList(std::vector<std::string> pFriends);
+
+        bool operator<(const Concert& other) const;
+        friend std::ostream& operator<<(std::ostream& stream, const Concert& object);
+
+        private:
+        std::string concertName;
+        std::vector<std::string> friends;
+        int desire;
+        std::tm date;
+        std::string sDate;
+};
+#endif
